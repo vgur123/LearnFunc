6a844e1225e603d6f39ec769956f52742f3cefef
vgur <vgur1963@yandex.ru>
2023-01-21 09:38:47 +0300

    FlowFunctionImpl

--- src/main/java/scenario/service/flow/impl/FlowFunctionImpl.java
+++ src/main/java/scenario/service/flow/impl/FlowFunctionImpl.java
@@ -3,21 +3,34 @@ package scenario.service.flow.impl;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.json.JSONWriter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Mono;
 import scenario.MDCUtils;
 import scenario.context.FlowContext;
 import scenario.service.flow.FlowFunction;
 import scenario.service.flow.sub.SubFlowFunction;
 
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 @Slf4j
 public abstract class FlowFunctionImpl  <RQ, RS, CTX extends FlowContext<RQ, RS>> implements FlowFunction <RQ, RS>{
-    protected SubFlowFunction<CTX> subFlow = SubFlowFunction.empty();
+    protected Logger loggerCtx =  LoggerFactory.getLogger(getClass());
+    protected Supplier<RS> rsConstructor = new ClassConstructorSupplier<>(GenericUtils.resolveTypeArgument(getClass(), FlowFunctionImpl.class, 1));
+    protected Supplier<RS> ctxConstructor = new ClassConstructorSupplier<>(GenericUtils.resolveTypeArgument(getClass(), FlowFunctionImpl.class, 2));
+    protected Consumer<CTX> fillContextDefault = this::fillContextDefault;
     protected Function<RQ, CTX> createContext = this::createContext;
-    protected Consumer<CTX> rqLogger = this::rqLogger;
-    protected Consumer<CTX> rsLogger = this::rsLogger;
+    protected Consumer<CTX> rqLogger = FlowRqLoggerImpl.getInstanceDefauly();
+    protected Consumer<CTX> rsLogger = FlowRsLoggerImpl.getInstanceDefauly();
+    protected SubFlowFunction<CTX> subFlow = SubFlowFunction.empty();
+    protected BiConsumer<CTX, Throwable> applyError = this::applyError;
+    protected JsonWriter<Object> jsonWriter = new JSONWriter();
+
+
 
     protected void rqLogger(CTX ctx) {
         log.info("Request is {}", ctx.getRequest() );
@@ -60,5 +73,14 @@ public abstract class FlowFunctionImpl  <RQ, RS, CTX extends FlowContext<RQ, RS>
         ctx.setTime(System.currentTimeMillis()-ctx.getStart());
         rsLogger.accept(ctx);
         return ctx.getResponse();
+    }
+
+    private void fillContextDefault(CTX ctx) {
+    }
+
+    private void applyError(CTX ctx, Throwable throwable) {
+    }
+
+    private class JsonWriter<T> {
     }
 }
